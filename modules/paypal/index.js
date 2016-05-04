1dc5d49d1705325fc419d86a474455eb9edf7ad5
Hervé Tutuaku <herve.tutuaku@gmail.com>
2016-05-04 22:00:44 +0200

    [feature] define config route in module

--- modules/paypal/index.js
+++ modules/paypal/index.js
@@ -1,26 +1,86 @@
 /**
- * Module exports.
- * @public
+ * Dependencies
+ * @type {[type]}
  */
-exports.version = "0.0.1";
-exports.init 	= init;
-exports.login 	= login;
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest
 
 
-var urls = {};
+/**
+ * [paypal description]
+ * @type {Object}
+ */
+var paypal = {};
+
+paypal.version = "0.0.1";
+paypal.config = {
+	'clientID': '',
+	'secret': ''
+};
+paypal.urls = [{}];
 
 /**
  * Simple test method to test the node module
  * @return void
  */
-function init () {
+paypal.init = function (app, dir) {
 	console.log("[paypal] Paypal module ready to use");
+	app.get('/config.json', function (req, res) {
+		res.sendFile(dir + '/config.json');
+	});
+};
+
+/**
+ * [getConfig description]
+ * @return {[type]} [description]
+ */
+paypal.getConfig = function (url) {
+	var url = typeof url !== 'undefined' ?  url : null;
+	if (null) {
+		return;
+	}
+
+	var request = new XMLHttpRequest();
+	request.open('GET', url + 'config.json', true);
+
+	request.onload = function() {
+		if (request.status >= 200 && request.status < 400) {
+			// Success!
+			var data = JSON.parse(request.responseText);
+			console.log("\033[0;34m[paypal] Config loaded with success\033[0m");
+			paypal.config = data;
+
+			// paypal.showConfig();
+		} else {
+			// Error!
+			console.log("error");
+		}
+	};
+
+	request.onerror = function() {
+	  // There was a connection error of some sort
+	};
+
+	request.send();
+};
+
+paypal.showConfig = function () {
+	console.log(paypal.config);
 };
 
 /**
  * [login description]
  * @return {[type]} [description]
  */
-function login () {
+paypal.login = function () {
 	console.log("[paypal] Login method");
 };
+
+/**
+ * Module exports.
+ * @public
+ */
+exports.version 	= paypal.version;
+exports.init 		= paypal.init;
+exports.login 		= paypal.login;
+exports.getConfig 	= paypal.getConfig;
+exports.showConfig 	= paypal.showConfig;
