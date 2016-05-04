2a1301db0388830117da871c884ded8763323b0a
Hervé Tutuaku <herve.tutuaku@gmail.com>
2016-05-04 18:24:24 +0200

    [fix] fix server.js

--- app/server.js
+++ app/server.js
@@ -2,18 +2,14 @@ var express = require('express');
 var app = express();
 var http = require('http').Server(app);
 
-app.use("/styles", express.static(__dirname + '/styles'));
-app.use("/scripts", express.static(__dirname + '/scripts'));
+app.use("/css", express.static(__dirname + '/css'));
+app.use("/js", express.static(__dirname + '/js'));
 app.use("/images", express.static(__dirname + '/images'));
-app.use("/vendor", express.static(__dirname + '/vendor'));
+app.use("/bower_components", express.static(__dirname + '/bower_components'));
 
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/index.html');
 });
-
-app.get('/pay', function (req, res) {
-	// body...
-})
 
 http.listen(8000, function() {
   console.log('listening on *:8000');
