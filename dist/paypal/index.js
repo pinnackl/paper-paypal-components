2fca537c0e3daa27da0b7d7b1ac8053d06d9d562
Hervé Tutuaku <herve.tutuaku@gmail.com>
2016-06-28 17:11:41 +0200

    [package] package paper-paypal-component

---
+++ dist/paypal/index.js
@@ -0,0 +1,263 @@
+/**
+ * Dependencies
+ * @type {[type]}
+ */
+var express 		= require('express');
+var XMLHttpRequest 	= require("xmlhttprequest").XMLHttpRequest
+var btoa 			= require("btoa");
+var url 			= require('url');
+var path 			= require('path');
+
+/**
+ * [paypal description]
+ * @type {Object}
+ */
+var paypal = {};
+var helper = {};
+
+paypal.version = "0.0.1";
+paypal.config = {
+	'clientID': '',
+	'secret': ''
+};
+paypal.urls = {
+	auth: {
+		url: "https://api.sandbox.paypal.com/v1/oauth2/token",
+		headers: [
+			{
+				header: "Accept",
+				value: "application/json"
+			}
+		],
+		data: {grant_type: "client_credentials"}
+	},
+	payment: {
+		url: "https://api.sandbox.paypal.com/v1/payments/payment",
+		headers: [
+			{
+				header: "Content-Type",
+				value: "application/json"
+			}
+		],
+		data: {}
+	}
+};
+
+/**
+ * Simple test method to test the node module
+ * @return void
+ */
+paypal.init = function (app, dir) {
+	console.log("[paypal] Paypal module ready to use");
+
+	app.get('/config.json', function (req, res) {
+		res.sendFile(dir + '/config.json');
+	});
+
+	/**
+	 * Create namespace paypal to load dependencies
+	 * @param  {[type]} req  [description]
+	 * @param  {[type]} res) {		res.sendFile(path.resolve('dist/paypal/paypal-api.js'));	} [description]
+	 * @return {[type]}      [description]
+	 */
+	app.get('/paypal/paypal-api.js', function (req, res) {
+		res.sendFile(path.resolve('dist/paypal/paypal-api.js'));
+	});
+
+	app.get('/paypal/gettoken', function (req, res) {
+		var url = req.protocol + '://' + req.get('host');
+		paypal.getConfig(url, req, res);
+	});
+
+	app.post('/paypal/payment', function (req, res) {
+		var data = req.body.transaction;
+		var res = res;
+
+		paypal.payment({
+			type: 'POST',
+			data: data,
+			callback: function (response) {
+				res.status(response.status);
+				res.setHeader('content-Type', 'application/json');
+				res.send(response.responseText);
+			},
+			failure: function (response) {
+				res.status(response.status);
+				res.setHeader('content-Type', 'application/json');
+				res.send({"error": "transaction error"});
+			},
+			headers: [
+				{header: 'Authorization', value: req.get('Authorization')},
+				{header: 'Content-Type', value: 'application/json'}
+			]
+		});
+	});
+
+	// FIXME : define all route
+	// ...
+};
+
+/**
+ * [getConfig description]
+ * @return {[type]} [description]
+ */
+paypal.getConfig = function (url, req, res) {
+	var url = typeof url !== 'undefined' ?  url : null;
+	var req = typeof req !== 'undefined' ?  req : null;
+	var res = typeof res !== 'undefined' ?  res : null;
+
+	if (null) {
+		return;
+	}
+
+	// FIXME: Assure the the url is the good one
+	helper.ajax({
+		url: url + '/config.json',
+		type: "GET",
+		callback: function (request) {
+			var data = JSON.parse(request.responseText);
+			console.log("\033[0;34m[paypal] Config loaded with success\033[0m");
+			paypal.config = data;
+			
+			// Get the oauth token
+			// And return the json response to the api
+			paypal.oauth({
+				endPoint: paypal.urls.auth.url,
+				user: paypal.config.clientID,
+				password: paypal.config.secret,
+				callback: function (request) {
+					if (JSON.parse(request.responseText)) {
+						res.setHeader('Content-Type', 'application/json');
+						res.send(JSON.stringify(JSON.parse(request.responseText)))
+					}
+				}
+			});
+		}
+	});
+};
+
+/**
+ * [showConfig description]
+ * @return {[type]} [description]
+ */
+paypal.showConfig = function () {
+	console.log(paypal.config);
+};
+
+/**
+ * [oauth description]
+ * @param  {[type]} param [description]
+ * @return {[type]}       [description]
+ */
+paypal.oauth = function (param) {
+	var endPoint 	= typeof param.endPoint !== 'undefined' ? param.endPoint : paypal.urls.auth.url;
+	var user 		= typeof param.user !== 'undefined' ? param.user : null;
+	var password 	= typeof param.password !== 'undefined' ? param.password : null;
+	var callback 	= typeof param.callback !== 'undefined' ? param.callback : function () {};
+
+	helper.ajax({
+		url: endPoint + '?grant_type=client_credentials',
+		type: 'POST',
+		callback: callback,
+		user: user,
+		password: password
+	});
+};
+
+/**
+ * [payment description]
+ * @param  {[type]} param [description]
+ * @return {[type]}       [description]
+ */
+paypal.payment = function (param) {
+	var endPoint 	= typeof param.endPoint !== 'undefined' ? param.endPoint : paypal.urls.payment.url;
+	var headers		= typeof param.headers !== 'undefined' ? param.headers : paypal.urls.payment.headers;
+	var data 		= typeof param.data !== 'undefined' ? param.data : {};
+	var callback	= typeof param.callback !== 'undefined' ? param.callback : function () {};
+	var failure		= typeof param.failure !== 'undefined' ? param.failure : function () {};
+
+	helper.ajax({
+		url: endPoint,
+		type: 'POST',
+		data: data,
+		callback: callback,
+		failure: failure,
+		headers: headers
+	});
+};
+
+/**
+ * [ajax description]
+ * @param  {[type]} url
+ * @param  {[type]} type
+ * @param  {[type]} data
+ * @param  {[type]} callback
+ * @param  {[type]} failure
+ * @param  {[type]} headers
+ * @return {[type]}
+ */
+helper.ajax = function (param) {
+	var url 		= typeof param.url !== 'undefined' ? param.url  : "/";
+	var type 		= typeof param.type !== 'undefined' ? param.type  : "GET";
+	var data 		= typeof param.data !== 'undefined' ? helper.encodeData(param.data)  : helper.encodeData({});
+	var callback 	= typeof param.callback !== 'undefined' ? param.callback  : function () {};
+	var failure 	= typeof param.failure !== 'undefined' ? param.failure  : function () {};
+	var headers 	= typeof param.headers !== 'undefined' ? param.headers  : [{'header': 'X-Requested-With', 'value': 'XMLHttpRequest'}];
+	var user 		= typeof param.user !== 'undefined' ? param.user  : null;
+	var password	= typeof param.password !== 'undefined' ? param.password  : null;
+
+	var request = new XMLHttpRequest();
+	request.open(type, url, true, user, password);
+	request.withCredentials = true;
+
+	for (var i = 0; i < headers.length; i++) {
+		request.setRequestHeader(headers[i].header, headers[i].value);
+	};
+
+	if (data !== "" && type.toLowerCase() == "post") {
+		request.setRequestHeader("Content-type", "application/json");
+
+		var data = JSON.stringify(param.data);
+	}
+
+	request.onload = function() {
+		if (request.status >= 200 && request.status < 400) {
+			// Success!
+			callback(request);
+		} else {
+			// Error!
+			console.log(request.status, " error: ", request.responseText);
+			failure(request);
+		}
+	};
+
+	request.onerror = function() {
+	  // There was a connection error of some sort
+	  console.log(request.status);
+	};
+
+	request.send(data);
+};
+
+/**
+ * [encodeData description]
+ * @param  {[type]} data [description]
+ * @return {[type]}      [description]
+ */
+helper.encodeData = function (data) {
+    var queryString = Object.keys(data).map(function(key) {
+            return encodeURIComponent(key) + '=' +
+                encodeURIComponent(data[key]);
+        }).join('&');
+    return encodeURI(queryString);
+}
+
+/**
+ * Module exports.
+ * @public
+ */
+exports.version 	= paypal.version;
+exports.init 		= paypal.init;
+exports.login 		= paypal.login;
+exports.getConfig 	= paypal.getConfig;
+exports.showConfig 	= paypal.showConfig;
---
+++ dist/paypal/paypal-api.js
@@ -0,0 +1,224 @@
+/**
+ * [description]
+ * @param  {[type]} window   [description]
+ * @param  {[type]} document [description]
+ * @return {[type]}          [description]
+ */
+(function (document) {
+	"use strict";
+
+	var paypalApi = {};
+	var helper = {};
+
+	/****************************************************************************************************\
+	********************************************PAYPAL METHODS********************************************
+	\****************************************************************************************************/
+
+	/**
+	 * [accessToken description]
+	 * @type {Object}
+	 */
+	paypalApi.accessToken = null;
+
+	/**
+	 * [approvalUrl description]
+	 * @type {Object}
+	 */
+	paypalApi.approvalUrl = null;
+	
+	/**
+	 * [getToken description]
+	 * @param  {[type]} baseUrl [description]
+	 * @return {[type]}         [description]
+	 */
+	paypalApi.getToken = function (baseUrl) {
+		var url  = helper.getUrl("/paypal/gettoken");
+
+		helper.ajax({
+			url: url,
+			type: "GET",
+			callback: function (response) {
+				var parsedResponse = JSON.parse(response.responseText);
+				paypalApi.accessToken = parsedResponse.token_type + " " + parsedResponse.access_token;
+			},
+			headers: [
+				{'header': 'X-Requested-With', 'value': 'XMLHttpRequest'},
+				{'header': 'Content-Type', 'value': 'application/x-www-form-urlencoded'}
+			]
+		})
+	};
+
+	paypalApi.sendPayment = function () {
+		var url  = helper.getUrl("/paypal/payment");
+		var cancelUrl = helper.getUrl("/?cancel=true");
+		var returnUrl = helper.getUrl("/?success=true");
+
+		helper.ajax({
+			url: url,
+			type: "POST",
+			data: { 
+				"transaction" :	{
+					"transactions": [{
+						"amount": {
+							"currency":"USD",
+							"total":"12"
+						},
+						"description":"creating a payment"
+					}],
+					"payer": {
+						"payment_method":"paypal"
+					},
+					"intent":"sale",
+					"redirect_urls": {
+						"cancel_url": cancelUrl,
+						"return_url": returnUrl
+					}
+				},
+			},
+			callback: function (response) {
+				var parsedResponse = JSON.parse(response.responseText);
+				var aLinks = parsedResponse.links;
+				for (var i = 0; i < aLinks.length; i++) {
+				    if(aLinks[i].rel == "approval_url") {
+						paypalApi.approvalUrl = aLinks[i].href;	
+						break;			    	
+				    }
+				}
+				window.location.href = paypalApi.approvalUrl;
+			},
+			headers: [{
+				header: 'Authorization',
+				value: paypalApi.accessToken
+			}]
+		})
+	};
+
+	paypalApi.executePayment = function () {
+		var url  = helper.getUrl("/paypal/execute");
+		var query = (window.location.search || '?').substr(1);
+        var map = {};
+	    query.replace(/([^&=]+)=?([^&]*)(?:&+|$)/g, function(match, key, value) {
+	        (map[key] = map[key] || []).push(value);
+	    });
+
+		helper.ajax({
+			url: url,
+			type: "POST",
+			data: { 
+				"payer_id" : map.PayerId,
+			},
+			callback: function (response) {
+				var parsedResponse = JSON.parse(response.responseText);
+				paypalApi.paymentId = parsedResponse.id;
+				paypalApi.state = parsedResponse.state;
+			},
+			headers: [{
+				header: 'Authorization',
+				value: paypalApi.accessToken
+			}]
+		})
+	};
+
+
+
+
+	/****************************************************************************************************\
+	********************************************HELPER METHODS********************************************
+	\****************************************************************************************************/
+
+	/**
+	 * [ajax description]
+	 * @param  {[type]} url
+	 * @param  {[type]} type
+	 * @param  {[type]} data
+	 * @param  {[type]} callback
+	 * @param  {[type]} failure
+	 * @param  {[type]} headers
+	 * @return {[type]}
+	 */
+	helper.ajax = function (param) {
+		var url 		= typeof param.url !== 'undefined' ? param.url  : "/";
+		var type 		= typeof param.type !== 'undefined' ? param.type  : "GET";
+		var data 		= typeof param.data !== 'undefined' ? helper.encodeData(param.data)  : helper.encodeData({});
+		var callback 	= typeof param.callback !== 'undefined' ? param.callback  : function () {};
+		var failure 	= typeof param.failure !== 'undefined' ? param.failure  : function () {};
+		var headers 	= typeof param.headers !== 'undefined' ? param.headers  : [{'header': 'X-Requested-With', 'value': 'XMLHttpRequest'}];
+		var user 		= typeof param.user !== 'undefined' ? param.user  : null;
+		var password	= typeof param.password !== 'undefined' ? param.password  : null;
+
+		var request = new XMLHttpRequest();
+		request.open(type, url, true, user, password);
+
+		for (var i = 0; i < headers.length; i++) {
+			request.setRequestHeader(headers[i].header, headers[i].value);
+		};
+
+		if (data !== "" && type.toLowerCase() == "post") {
+			request.setRequestHeader("conte", "application/json");
+			request.setRequestHeader("content-type", "application/json");
+
+			var data = JSON.stringify(param.data);
+		}
+
+		request.onload = function() {
+			if (request.status >= 200 && request.status < 400) {
+				// Success!
+				callback(request);
+			} else {
+				// Error!
+				console.log(request.status, " error: ", request.responseText);
+				failure(request);
+			}
+		};
+
+		request.onerror = function() {
+		  // There was a connection error of some sort
+		};
+
+		request.send(data);
+	};
+
+	/**
+	 * [encodeData description]
+	 * @param  {[type]} data [description]
+	 * @return {[type]}      [description]
+	 */
+	helper.encodeData = function (data) {
+	    var queryString = Object.keys(data).map(function(key) {
+	            return encodeURIComponent(key) + '=' +
+	                encodeURIComponent(data[key]);
+	        }).join('&');
+
+	    return encodeURI(queryString);
+	}
+
+	helper.encodeFormData = function (data) {
+
+	}
+
+	/**
+	 * [getUrl description]
+	 * @param  {[type]} path [description]
+	 * @return {[type]}      [description]
+	 */
+	helper.getUrl = function (path) {
+		// Get the window location
+		var l = window.location;
+
+		var protocol = l.protocol;
+		var host = l.hostname;
+		var port = l.port;
+		var path = typeof path !== 'undefined' ? path : l.pathname;
+		var hash = l.hash;
+
+		var url = protocol + "//" + host + (port !== "" ? ":" + port : null) + path + hash;
+
+		return url;
+	};
+
+	/**
+	 * Register api
+	 */
+	window.paypalApi = paypalApi;
+	window.paypalHelper = helper;
+})(document);
